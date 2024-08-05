5e3a073e0423c84d104e60af4a124cc990552870
Łukasz Turowski <Turek695@gmail.com>
2024-08-05 21:46:26 +0200

    update default layout
    add some styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-	<div>
-		<nav>
+	<div class="page_layout">
+		<nav class="page_nav shrink-0">
 			<NuxtLink to="/">Home</NuxtLink>
 		</nav>
-		<main>
+		<main class="page_main-content shrink-0">
 			<slot />
 		</main>
-		<footer>
+		<footer class="page_footer shrink-0">
 			<span>copyright Turek</span>
 		</footer>
 	</div>
@@ -15,5 +15,18 @@
 <script setup>
 </script>
 
-<style>
+<style scoped>
+.page_layout {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+}
+
+.page_main-content {
+	height: 100%;
+}
+
+.page_footer {
+	margin-top: auto;
+}
 </style>
